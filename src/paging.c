a6f94966efed1da614ce52c8d0a8735ae3b219cc
William Hao <86330766+whao37@users.noreply.github.com>
2024-05-07 00:11:11 -0400

    Update paging.c

--- src/paging.c
+++ src/paging.c
@@ -6,27 +6,7 @@
  /* The frame table pointer. You will set this up in system_init. */
 fte_t *frame_table;
 
-/*  --------------------------------- PROBLEM 2 --------------------------------------
-    In this problem, you will initialize the frame table.
 
-    The frame table will be located at physical address 0 in our simulated
-    memory. You will first assign the frame_table global variable to point to
-    this location in memory. You should zero out the frame table, in case for
-    any reason physical memory is not clean.
-
-    You should then mark the first entry in the frame table as protected. We do
-    this because we do not want our free frame allocator to give out the frame
-    used by the frame table.
-
-    HINTS:
-        You will need to use the following global variables:
-        - mem: Simulated physical memory already allocated for you.
-        - PAGE_SIZE: The size of one page.
-        You will need to initialize (set) the following global variable:
-        - frame_table: a pointer to the first entry of the frame table
-
-    -----------------------------------------------------------------------------------
-*/
 void system_init(void) {
     /*
      * 1. Set the frame table pointer to point to the first frame in physical
@@ -52,20 +32,6 @@ void system_init(void) {
 
 }
 
-/*  --------------------------------- PROBLEM 3 --------------------------------------
-    This function gets called every time a new process is created.
-    You will need to allocate a new page table for the process in memory using the
-    free_frame function so that the process can store its page mappings. Then, you
-    will need to store the PFN of this page table in the process's PCB.
-
-    HINTS:
-        - Look at the pcb_t struct defined in pagesim.h to know what to set inside.
-        - You are not guaranteed that the memory returned by the free frame allocator
-        is empty - an existing frame could have been evicted for our new page table.
-        - As in the previous problem, think about whether we need to mark any entries
-        in the frame_table as protected after we allocate memory for our page table.
-    -----------------------------------------------------------------------------------
-*/
 void proc_init(pcb_t *proc) {
     /*
      * 1. Call the free frame allocator (free_frame) to return a free frame for
@@ -92,50 +58,10 @@ void proc_init(pcb_t *proc) {
 
 }
 
-/*  --------------------------------- PROBLEM 4 --------------------------------------
-    Swaps the currently running process on the CPU to another process.
-
-    Every process has its own page table, as you allocated in proc_init. You will
-    need to tell the processor to use the new process's page table.
-
-    HINTS:
-        - Look at the global variables defined in pagesim.h. You may be interested in
-        the definition of pcb_t as well.
-    -----------------------------------------------------------------------------------
- */
 void context_switch(pcb_t *proc) {
     PTBR = proc->saved_ptbr;
 }
 
-/*  --------------------------------- PROBLEM 5 --------------------------------------
-    Takes an input virtual address and returns the data from the corresponding
-    physical memory address. The steps to do this are:
-
-    1) Translate the virtual address into a physical address using the page table.
-    2) Go into the memory and read/write the data at the translated address.
-
-    Parameters:
-        1) address     - The virtual address to be translated.
-        2) rw          - 'r' if the access is a read, 'w' if a write
-        3) data        - One byte of data to write to our memory, if the access is a write.
-                         This byte will be NULL on read accesses.
-
-    Return:
-        The data at the address if the access is a read, or
-        the data we just wrote if the access is a write.
-
-    HINTS:
-        - You will need to use the macros we defined in Problem 1 in this function.
-        - You will need to access the global PTBR value. This will tell you where to
-        find the page table. Be very careful when you think about what this register holds!
-        - On a page fault, simply call the page_fault function defined in page_fault.c.
-        You may assume that the pagefault handler allocated a page for your address
-        in the page table after it returns.
-        - Make sure to set the referenced bit in the frame table entry since we accessed the page.
-        - Make sure to set the dirty bit in the page table entry if it's a write.
-        - Make sure to update the stats variables correctly (see stats.h)
-    -----------------------------------------------------------------------------------
- */
 uint8_t mem_access(vaddr_t address, char rw, uint8_t data) {
 
 
