a1220f6f79eb52b5637942521bfb4d9e7bc56e51
William Hao <86330766+whao37@users.noreply.github.com>
2024-05-07 00:10:22 -0400

    Update page_fault.c

--- src/page_fault.c
+++ src/page_fault.c
@@ -2,28 +2,6 @@
 #include "swapops.h"
 #include "stats.h"
 
-/*  --------------------------------- PROBLEM 6 --------------------------------------
-    Page fault handler.
-
-    When the CPU encounters an invalid address mapping in a page table,
-    it invokes the OS via this handler.
-
-    Your job is to put a mapping in place so that the translation can
-    succeed. You can use free_frame() to make an available frame.
-    Update the page table with the new frame, and don't forget
-    to fill in the frame table.
-
-    Lastly, you must fill your newly-mapped page with data. If the page
-    has never mapped before, just zero the memory out. Otherwise, the
-    data will have been swapped to the disk when the page was
-    evicted. Call swap_read() to pull the data back in.
-
-    HINTS:
-         - You will need to use the global variable current_process when
-           setting the frame table entry.
-
-    ----------------------------------------------------------------------------------
- */
 void page_fault(vaddr_t address) {
     /* First, split the faulting address and locate the page table entry */
 
