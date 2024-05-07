d5861e46a90a1610a7be9d1984a1fcdbb9853e4f
William Hao <86330766+whao37@users.noreply.github.com>
2024-05-07 00:11:19 -0400

    Update stats.c

--- src/stats.c
+++ src/stats.c
@@ -4,18 +4,6 @@
 /* The stats. See the definition in stats.h. */
 stats_t stats;
 
-/*  --------------------------------- PROBLEM 9 --------------------------------------
-    Calculate any remaining statistics to print out.
-
-    You will need to include code to increment many of these stats in
-    the functions you have written for other parts of the project.
-
-    Use this function to calculate any remaining stats, such as the
-    average access time (AAT).
-
-    You may find the #defines in the stats.h file useful.
-    -----------------------------------------------------------------------------------
-*/
 void compute_stats() {
     stats.accesses = stats.writes + stats.reads;
     uint64_t total_memory_read_time = MEMORY_READ_TIME * stats.accesses;
